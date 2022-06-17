d34572a58c5c41f069bec4abbd08aceab75aadc3
Vinod Borse <borse002@gmail.com>
2022-06-18 00:07:16 +0530

    Spring framework configuration using java file
    Replace Spring framework XML configuration to java file configuration

---
+++ spring-config-app/src/main/java/com/vinod/AppConfig.java
@@ -0,0 +1,13 @@
+package com.vinod;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public DemoService getDemo() {
+		return new DemoService();
+	}
+}
---
+++ spring-config-app/src/main/java/com/vinod/DemoService.java
@@ -0,0 +1,11 @@
+package com.vinod;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoService {
+
+	public void m1() {
+		System.out.println("Hello DON!");
+	}
+}
